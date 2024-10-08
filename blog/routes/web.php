052334112f560ac7acf9201fe07e0390f1fc4564
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-10-08 21:24:17 +0800

    ss

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,39 +1,21 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-// Home route
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Dashboard route (protected by auth middleware)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Profile routes (protected by auth middleware)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Post and Comment routes (protected by auth middleware)
-    Route::resource('posts', PostController::class);
-    Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
 });
 
-// Authentication routes (login, register, etc.)
 require __DIR__.'/auth.php';
-
-//admin
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
-
-    Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
-    Route::post('comments/{comment}/report', [CommentController::class, 'report'])->name('comments.report');
-    Route::post('users/{user}/report', [UserController::class, 'reportUser'])->name('users.report');
-});
+require __DIR__.'/admin-auth.php';
